8d562a1c5470565c8480eb6ab456874e26af9759
dspv <dspv@me.com>
2020-10-03 18:35:20 +0300

    remote, origin, etc.

--- git.sh
+++ git.sh
@@ -20,3 +20,18 @@ git log
 # then use *.pub file to add a new SSH key to GitHub account
 ssh-keygen -t rsa -b 4096 -C "dmitriy@solodukha.com"
 pbcopy < [file] # to copy to clipboard
+
+# when create a new repo
+# we need to create a repo first on GitHub
+# then
+git init # in the new repo on local machine
+
+# add a place where to push
+git remote add origin https://github.com/dspv/demo-repo-2.git 
+
+# take a look at all the remote hosts
+git remote -v
+
+# doc
+https://training.github.com/downloads/github-git-cheat-sheet/
+
