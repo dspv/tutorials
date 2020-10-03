36b9a9d93a7328a2e7f530f1138bc43f300e460f
dspv <dspv@me.com>
2020-10-03 19:46:44 +0300

    Branches + some other things

--- git.sh
+++ git.sh
@@ -15,6 +15,8 @@ git commit -m "Initial commit" -m "Some description"
 # history of commits
 git log
 
+
+
 ### Some SSH Magic ###
 # generate a new SSH key (email to account)
 # then use *.pub file to add a new SSH key to GitHub account
@@ -34,4 +36,55 @@ git remote -v
 
 # doc
 https://training.github.com/downloads/github-git-cheat-sheet/
+
+
+
+### Branches ###
+# check all the branches
+git branch
+
+# create a new branch
+git checkout -b feature
+git checkout -b feature-111 # issue number
+git checkout -b feature-readme-instructions
+
+# then deploy
+git status
+git add .
+git commit -m "comment" -m 'description'
+
+## Merging branches
+# take a look on cnahges
+git diff [branch name]
+git diff feature-readme-001
+
+# merge
+git merge feature-readme-001
+
+# But #
+# But #
+# there's another way
+git checkout feature-readme-001
+git status
+git push -u origin feature-readme-001
+
+# then
+# we can delete branch
+git branch -d feature-readme-001
+# and check the branchs
+git branch
+
+### And one again
+git status
+git diff
+# (git add .)
+# add and commit and the same time 
+# (only works for modified files)
+git commit -am "comment"
+git checkout master
+
+git merge master
+
+# unstage changes
+git reset
 
