825b49d7f6ab2be5dfe4a148bf705e7f7d71c32a
dspv <dspv@me.com>
2020-10-03 16:12:21 +0300

    added some things to git.sh

--- git.sh
+++ git.sh
@@ -6,9 +6,17 @@ git status
 
 # add files to track
 git add index.html
+# add everything in this folder
+git add .
 
 # commit changes
-git commit -m "Initial commit"
+git commit -m "Initial commit" -m "Some description"
 
 # history of commits
 git log
+
+### Some SSH Magic ###
+# generate a new SSH key (email to account)
+# then use *.pub file to add a new SSH key to GitHub account
+ssh-keygen -t rsa -b 4096 -C "dmitriy@solodukha.com"
+pbcopy < [file] # to copy to clipboard
